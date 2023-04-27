57b2568f9a959c1115793edb6c28ae4331b01f8b
Lebarbanchon Valentin <valentin.lebarbanchon@ecole.ensicaen.fr>
2023-04-27 08:51:46 +0200

    9-create-buttonbuilder Change colors & constants to be static const, Added button_builder & logger

--- lib/src/themes/constants.dart
+++ lib/src/themes/constants.dart
@@ -2,29 +2,48 @@ import 'package:flutter/material.dart';
 
 @immutable
 class AppConstants {
-  final GradientAlignments gradientAlignments = GradientAlignments();
-  final GradientRadius gradientRadius = GradientRadius();
-  final BorderRadius borderRadius = BorderRadius();
-  final ButtonSize buttonSize = ButtonSize();
+  GradientAlignments get gradientAlignments => const GradientAlignments();
 
-  AppConstants();
+  GradientRadius get gradientRadius => const GradientRadius();
+
+  BorderRadius get borderRadius => const BorderRadius();
+
+  ButtonSize get buttonSize => const ButtonSize();
+
+  SizedBoxSize get sizedBoxSize => const SizedBoxSize();
+
+  const AppConstants();
 }
 
 class GradientAlignments {
-  final begin = const Alignment(-0.7, -1);
-  final end = const Alignment(0, 0.5);
-  final center = const Alignment(-0.4, 1.2);
+  static const begin = Alignment(-0.7, -1);
+  static const end = Alignment(0, 0.5);
+  static const center = Alignment(-0.4, 1.2);
+
+  const GradientAlignments();
 }
 
 class GradientRadius {
-  final global = 1.2;
+  static const global = 1.2;
+
+  const GradientRadius();
 }
 
 class BorderRadius {
-  final global = 25.0;
+  static const global = 35.0;
+
+  const BorderRadius();
 }
 
 class ButtonSize {
-  final width = 80.0;
-  final height = 80.0;
+  static const width = 100.0;
+  static const height = 100.0;
+
+  const ButtonSize();
+}
+
+class SizedBoxSize {
+  static const globalWidth = 50.0;
+
+  const SizedBoxSize();
 }
